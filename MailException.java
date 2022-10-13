2e07a7775aff569740fd4bd1a23b04f6715adfe6
Sushiljawale <113004919+Sushiljawale@users.noreply.github.com>
2022-10-13 17:36:47 +0530

    Commite LAbTest06
    Lab-Test06

---
+++ MailException.java
@@ -0,0 +1,39 @@
+package Exception.com;
+//Q.1)create your own invalidmailidexception and throw for not getting correct email id from user. 
+
+import java.util.Scanner;
+import java.util.regex.*;  
+
+public class MailException {
+
+	public static void main(String[] args) {
+	
+			String s1;
+			Scanner scan=new Scanner(System.in);
+			System.out.println ("Enter your email : ");
+			s1=scan.nextLine();
+			if(s1.endsWith("@")||s1.endsWith("."))
+				
+				System.out.println("invalid email id");    
+			
+			else
+			if((s1.contains("@")&& s1.contains(".")))
+					System.out.println("valid email id ");
+			
+			else
+				System.out.println("Invalid email id ");
+			
+			 try  
+		        {  
+		            // throw an object of user defined exception  
+		            throw new invalidmailidexception();  
+		        } 
+			 catch (invalidmailidexception ide)  
+		        {  
+					//System.out.println("Invalid email id ");
+		            // Print the message from MyException object  
+		           //  System.out.println(ide.getMessage());
+		        }  
+		}
+	
+	}
