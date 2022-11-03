88a92b1996c5586de17563a75ed4a14b7bef8738
Sushiljawale <113004919+Sushiljawale@users.noreply.github.com>
2022-11-03 12:16:03 +0530

    LabTest_09 Q.2
    commite....

---
+++ TreeFirstandLastValue.java
@@ -0,0 +1,31 @@
+//Q.2) Write a Java program to create a new tree set and add above ArrayLists in TreeSet.
+  //  Get the number of elements in  tree set and print first and last element of treeset.
+
+
+package Collection.com;
+import java.util.TreeMap;
+
+public class TreeFirstandLastValue {
+   public static void main(String[] args) {
+      System.out.println("(StudentList.java)!\n");
+      TreeMap <Integer, String>tMap = new TreeMap<Integer, String>();
+      tMap.put(1, "Sushil");
+      tMap.put(2, "Robet");
+      tMap.put(3, "Doney");
+      tMap.put(4, "Junior");
+      tMap.put(5, "Peter");
+      tMap.put(6, "Parker");
+      tMap.put(7, "Chris");
+      System.out.println("Keys of tree map: " + tMap.keySet());
+      System.out.println("Values of tree map: " + tMap.values());
+      System.out.println(" ");
+      
+      System.out.println(
+         "First element of ArryList Student: " + tMap.firstKey() + " Value: " + tMap.get(tMap.firstKey()));
+      System.out.println(
+         "Last element of ArryList Student:  " + tMap.lastKey() + " Value: "+ tMap.get(tMap.lastKey()));
+     
+   }
+}
+
+
