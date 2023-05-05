05770d8b0e196409515b08441e4c874f12eb76ff
Sushiljawale <113004919+Sushiljawale@users.noreply.github.com>
2023-05-05 11:38:50 +0530

    Delete LibraryManagmentSystem directory

--- LibraryManagmentSystem/src/main/java/com/library/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.library.entities.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-
-}
--- LibraryManagmentSystem/src/main/java/com/library/repositories/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.library.entities.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
